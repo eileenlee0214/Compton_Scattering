2f5b20d57dccdc755c220ac005a453b7d2890424
Daniel <daniel.caal08@gmail.com>
2026-05-25 15:56:59 -0400

    finalized photon physics

--- photonsandelectrons.py
+++ photonsandelectrons.py
@@ -1,6 +1,7 @@
 Web VPython 3.2
 
 restart = False
+paused = False
 scene.range = 10
 scene.autoscale = False
 scene.userrotate = False
@@ -27,13 +28,14 @@ class photon:
         self.ipos = ipos
         self.localtime = 0
         self.freq = c / wlength
+        self.dir = rotate(vec(1,0,0), angle = self.theta, axis = vec(0,0,1))
     
     def create_photon(self):
         self.curv = curve(color=vec(1,0,0))
         for i in range(400):
             dx = (i - 200) * dl
             y = dsine(dx, self.freq, 0, self.ipos.x)
-            pos = rotate(vec(dx, y, 0), angle = self.theta, axis = vec(0,0,1)) + self.ipos
+            pos = rotate(vec(dx, y, 0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) + self.ipos
             self.curv.append(pos = pos)
         self.pos = vec(self.ipos.x, self.ipos.y, 0)
         return self.curv
@@ -43,49 +45,44 @@ class photon:
             dx = (i - 200) * dl
             t = self.localtime * c    
             y = dsine(dx, self.freq, t, 0)
-            pos = rotate(vec(dx + t, y, 0), angle = self.theta, axis = vec(0,0,1)) + self.ipos
+            pos = rotate(vec(dx + t, y, 0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) + self.ipos
             self.curv.modify(i, pos = pos)
         self.localtime += dt
-        self.pos += rotate(vec(1,0,0), angle = self.theta, axis = vec(0,0,1)) * dt
+        self.pos += rotate(vec(1,0,0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) * dt
+        #spheree = sphere(pos = self.pos, radius = 0.1)
             
-    def collision(self):#calculating energy and updating frequency and electron velocity
+    def collision(self):#calculating energy and updating frequency and electron velocity from conservation of energy
         self.E_i = h * self.freq
         self.E_f = self.E_i / (1 + (self.E_i / (m * c**2)) * (1-cos(self.theta)))
         self.K = self.E_i - self.E_f
-        self.v_e = c * sqrt(1 - 1/(1 + self.K/(m*c**2)**2)) #electron deflected velocity
+        self.v_e = c * sqrt(1 - 1/(1 + self.K/(m*c**2))**2) #electron deflected velocity
         self.lambda_i = self.wlength
         self.lambda_f = h /(m*c) * (1 - cos(self.theta)) + self.lambda_i
         self.freq = c/self.lambda_f
         self.wlength = self.lambda_f
+        self.dir = rotate(self.dir, angle = self.theta, axis = vec(0,0,1))
+        
         
 class electron:
     def __init__(self, position, vel, phi):
         self.position = position
         self.vel = vel
         self.phi = phi
+        self.hit = vec(1,0,0)
         
     def create_electron(self):
-        self.sphere = sphere(pos=self.position, radius=0.5, color=color.cyan)
+        self.sphere = sphere(pos=self.position, radius=1, color=color.cyan)
         return self.sphere
         
     def electron_step(self):
         if self.vel != 0:
-            self.sphere.pos += vec(abs(cos(self.phi)), - abs(sin(self.phi)),0) * self.vel * dt 
-        
-    def find_phi(self, theta, E_i):
-        if theta == 0 or theta == pi:
-            return 0
-        else:
-            def f(phi):
-                return cot(phi) - (1 + E_i / (m*c**2)) * (sin(theta) / (1 + cos(theta)))
-            phi = 1.0
+            self.sphere.pos += self.hit * self.vel * dt
             
-            #newtons method
-            for i in range(20):
-                h = 0.0001
-                df = (f(phi + h) - f(phi - h)) / (2*h)
-                phi -= f(phi)/df
-            self.phi = phi
+    def collision(self, in_dir, out_dir, E_i, E_f): #electron direction from conservation of momentum
+        p_i = E_i * norm(in_dir)
+        p_f = E_f * norm(out_dir)
+        p_e = p_i - p_f
+        self.hit = norm(p_e)
  
 #making electrons
 electrons = []
@@ -109,6 +106,7 @@ for i in range(10):
 # widgets
 start = button(bind = run, text = 'run')
 reset = button(bind = reset, text = 'reset')
+pause = button(bind = pauser, text = 'pause')
 angle = slider(bind = change_angle, min = 0, max = 180, step = 1, value = angles)
 angle_text = wtext(text=f'{angles}\n')
 
@@ -120,6 +118,7 @@ def change_angle(evt):
 def reset():
     global restart
     global photons
+    global electrons
     restart = True
     n = 0
     for phot in photons:
@@ -130,25 +129,37 @@ def reset():
     for elect in electrons:
         elect.sphere.visible = False
         del elect
+    electrons = []
         
+def pauser():
+    global paused
+    paused = True
 
 def run():
+    global paused
+    paused = False
     while True:
         rate(1000)
         for phot in photons:
             phot.photon_step()
             for elect in electrons:
-                distance = mag(phot.pos + vec(100 * dl, 0, 0) - elect.sphere.pos)  
+                distance = mag(phot.pos - elect.sphere.pos)  
                 if distance < elect.sphere.radius: #detect collision with electron
-                    phot.theta += angles * pi / 180
+                    in_dir = phot.dir
+                    phot.theta = angles * pi / 180
                     phot.collision()
+                    out_dir = phot.dir
+                    elect.collision(in_dir, out_dir, phot.E_i, phot.E_f)
                     phot.localtime = 0
-                    phot.ipos = elect.sphere.pos 
+                    phot.ipos = elect.sphere.pos + phot.dir 
+                    phot.pos = vec(phot.ipos.x, phot.ipos.y, 0)
                     elect.vel = phot.v_e
-                    elect.find_phi(phot.theta, phot.E_i)
+                    #elect.find_phi(phot.theta, phot.E_i)
                     phot.freq = c / phot.lambda_f
         for elec in electrons:
             elec.electron_step() 
         if restart == True:
             restart = False
+            break
+        if paused == True:
             break
