d4ceeb55227a4e0cc945e988219e00f524a1a906
Daniel <daniel.caal08@gmail.com>
2026-05-28 19:56:50 -0400

    further fixes and trimming

--- diffcross_scattering.py
+++ diffcross_scattering.py
@@ -6,7 +6,6 @@ scene.userspin = False
 
 restart = False
 paused = False
-angles = 0
 freq = 10
 
 dl = 0.01
@@ -48,7 +47,7 @@ def kn_rejacc(Ei): #rejection acceptance method since the pdf is univertible
         samplex = random() * 2 - 1
         sampley = random()
 #    if random() > 0.5:
-#        return acos(samplex)
+#        return -acos(samplex)
 #    else: 
 #        return -acos(samplex)
     return acos(samplex)
@@ -58,8 +57,8 @@ def kn_rejacc(Ei): #rejection acceptance method since the pdf is univertible
 class photon:
     def __init__(self, wlength, ipos, theta):
         self.wlength = wlength
-        self.theta = theta
-        self.ipos = ipos
+        self.theta = theta #initial angle from positive x-axis
+        self.ipos = ipos #initial position
         self.localtime = 0 #since photons travel in a straight line from initial position, local time must be reset after every collision
         self.freq = c / wlength
         self.dir = rotate(vec(1,0,0), angle = self.theta, axis = vec(0,0,1))
@@ -80,28 +79,24 @@ class photon:
             dx = (i - 200) * dl
             t = self.localtime * c    
             y = dsine(dx, self.freq, t, 0)
-            pos = rotate(vec(dx + t, y, 0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) + self.ipos
-            self.curv.modify(i, pos = pos)
+            rotated = rotate(vec(dx + t, y, 0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1))
+            self.curv.modify(i, pos = rotated + self.ipos)
         self.localtime += dt
         self.pos += rotate(vec(1,0,0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) * dt
             
     def collision(self):#calculating energy and updating frequency and electron velocity from conservation of energy and momentum
         self.E_f = self.E_i / (1 + (self.E_i / (m * c**2)) * (1-cos(self.theta)))
         self.K = self.E_i - self.E_f
-        self.lambda_i = self.wlength
-        self.lambda_f = h /(m*c) * (1 - cos(self.theta)) + self.lambda_i #shift in wavelength
-        self.freq = c/self.lambda_f
-        self.wlength = self.lambda_f
+        self.wlength += h /(m*c) * (1 - cos(self.theta))
+        self.freq = c / self.wlength
         self.dir = rotate(self.dir, angle = self.theta, axis = vec(0,0,1))
         self.localtime = 0
         
         
 class electron:
-    def __init__(self, position, vel):
+    def __init__(self, position):
         self.position = position
-        self.vel = vel
-        self.hit = vec(1,0,0) # direction of collision
-        self.p = vec(0,0,0)
+        self.p = vec(0,0,0) #initial momentum
         
     def create_electron(self):
         self.sphere = sphere(pos=self.position, radius=1, color=color.cyan)
@@ -115,7 +110,6 @@ class electron:
         p_i = E_i / c * norm(in_dir)
         p_f = E_f / c * norm(out_dir)
         p_e = p_i - p_f
-        #self.hit = norm(p_e)
         self.p += p_e 
  
 #making electrons
@@ -124,7 +118,7 @@ electrons = []
 scene.bind('click', click_electron)
 
 def click_electron(evt):
-    e = electron(vec(evt.pos.x, evt.pos.y, 0), 0, 0)
+    e = electron(vec(evt.pos.x, evt.pos.y, 0))
     e.create_electron()
     electrons.append(e)
 
@@ -135,27 +129,14 @@ for i in range(10):
     p = photon(c/freq, vec(-10, 2*(i - 5), 0), 0)
     p.create_photon()
     photons.append(p)
-    
-#for i in range(1):
-#    p = photon(0.08, vec(-10, -10, 0), pi/4)
-#    p.create_photon()
-#    photons.append(p)
-
         
 # widgets
 start = button(bind = run, text = 'run')
 reset = button(bind = reset, text = 'reset')
 pause = button(bind = pauser, text = 'pause')
-#angle = slider(bind = change_angle, min = 0, max = 180, step = 1, value = angles)
-#angle_text = wtext(text=f'{angles}\n')
 frequency = slider(bind = change_freq, min = 1, max = 20, step = 1, value = freq)
 frequency_text = wtext(text = f'{freq}\n')
 click_text = wtext(text='Click on the canvas to place electrons, and then run')
-
-def change_angle(evt):
-    global angles 
-    angles = evt.value
-    angle_text.text = f'{angles}\n'
     
 def change_freq(evt):
     global freq 
@@ -182,8 +163,6 @@ def pauser():
     global paused
     paused = True
 
-#histogram = 
-
 def run():
     global paused
     paused = False
@@ -207,7 +186,6 @@ def run():
                     elect.collision(in_dir, out_dir, phot.E_i, phot.E_f)
                     phot.ipos = elect.sphere.pos + phot.dir # so we don't get repeated collisions we start it a little forward
                     phot.pos = vec(phot.ipos.x, phot.ipos.y, 0)
-                    phot.freq = c / phot.lambda_f
                     phot.E_i = h * phot.freq
         for elec in electrons:
             elec.electron_step() 
@@ -216,5 +194,3 @@ def run():
             break
         if paused == True:
             break
-        
-
