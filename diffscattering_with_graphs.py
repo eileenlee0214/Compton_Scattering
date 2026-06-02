19356d176647567625f40cce418761e9312a21ef
eileenlee0214 <iamliah1106@gmail.com>
2026-06-02 18:23:45 -0400

    added sections w what types of light, and fixed energy graph

--- diffscattering_with_graphs.py
+++ diffscattering_with_graphs.py
@@ -11,19 +11,33 @@ freq = 10
 dl = 0.01
 dt = 0.01
 
-sum_K = 0
-sum_Ei_curr = 0
 init_Ei = 0
-
 init_iM = 0
-sum_M_curr = 0
-sum_M = 0
 
 #for modeling purposes and simplicity, these constants will be set to 1
 c = 1 #speed of light
 m = 1 #electron mass
 h = 1 #planks constant
 r = 1 # electron radius
+
+NM_SCALE = 1
+
+GAMMA_THRESHOLD = 0.01
+XRAY_THRESHOLD = 10.0
+UV_THRESHOLD = 400.0
+VIS_THRESHOLD = 700.0
+
+def classify_photon(wl_nm):
+    if wl_nm < GAMMA_THRESHOLD:
+        return 'Gamma Ray'
+    elif wl_nm < XRAY_THRESHOLD:
+        return 'X-Ray'
+    elif wl_nm < UV_THRESHOLD:
+        return 'Ultraviolet'
+    elif wl_nm < VIS_THRESHOLD:
+        return 'Visible'
+    else:
+        return 'Infrared'
 
 energy_graph = graph(
     title = 'Conservation of Energy (E = hf)',
@@ -36,10 +50,10 @@ energy_graph = graph(
     align = 'right'
 )
 
-bar_Ei   = gvbars(graph=energy_graph, delta=0.5, color=color.magenta,  label='initial energy of photon')
+bar_Ei   = gvbars(graph=energy_graph, delta=0.5, color=color.magenta, label='initial energy of photon')
 bar_Ef   = gvbars(graph=energy_graph, delta=0.5, color=color.purple,  label='final energy of photon')
 bar_Ke   = gvbars(graph=energy_graph, delta=0.5, color=color.cyan,    label='kinetic energy of electron')
-bar_Etot = gvbars(graph=energy_graph, delta=0.5, color=color.blue,   label='check')
+bar_Etot = gvbars(graph=energy_graph, delta=0.5, color=color.blue,    label='check')
 
 def update_energy_graph(E_i, E_f, K):
     bar_Ei.plot(1, E_i)
@@ -48,7 +62,7 @@ def update_energy_graph(E_i, E_f, K):
     bar_Etot.plot(4, E_f + K) # should be equal to E_i ?
     energy_graph.ymax = max(E_i, E_f + K) * 1.5
 
-update_energy_graph(h * (c / (c/freq)), 0, 0)
+update_energy_graph(h * freq, 0, 0)
 
 #===== momentum graph
 momenta_graph = graph(
@@ -62,39 +76,44 @@ momenta_graph = graph(
     align = 'right'
 )
 
-bar_iM   = gvbars(graph=momenta_graph, delta=0.5, color=color.magenta,  label='initial momentum of photon')
+bar_iM   = gvbars(graph=momenta_graph, delta=0.5, color=color.magenta, label='initial momentum of photon')
 bar_fM   = gvbars(graph=momenta_graph, delta=0.5, color=color.purple,  label='final momentum of photon')
 bar_eM   = gvbars(graph=momenta_graph, delta=0.5, color=color.cyan,    label='momentum of electron after collision')
-bar_Mtot = gvbars(graph=momenta_graph, delta=0.5, color=color.blue,   label='check')
+bar_Mtot = gvbars(graph=momenta_graph, delta=0.5, color=color.blue,    label='check')
 
 def update_momenta_graph(iM, fM, eM):
     bar_iM.plot(1, iM)
     bar_fM.plot(2, fM)
     bar_eM.plot(3, eM)
     bar_Mtot.plot(4, fM + eM) # should be equal to iM ?
-    momenta_graph.ymax = max(iM, fM + eM) * 1.5
+    top = max(abs(iM), abs(fM + eM)) * 1.5
+    momenta_graph.ymax = top
+    momenta_graph.ymin = -top * 0.3
 
-update_momenta_graph(h / (c/freq) , 0, 0) ## fix this to be momenta, not energy
-
-## checks for the sum aren't working ?!?!?
+update_momenta_graph(h * freq, 0, 0)
 
 # ======end
+
+def update_wl_label(wl_sim):
+    wl_nm = wl_sim * NM_SCALE
+    name = classify_photon(wl_nm)
+    wl_label.text = f'  lambda = {wl_nm:.3f} nm  [{name}]\n'
 
 #wave packet form
 def dsine(x, freq, phase, dis):
     return exp(-(x**2)) * sin(freq * (x - dis + phase))
-    
-    
+
+
 def kn_rejacc(Ei): #rejection acceptance method since the pdf is univertible
     g = Ei / (m*c**2) #photon energy per electron rest mass
     def diff_cross(x):
         l_ratio = 1 / (1 + g * (1 - x))#x = cos (theta), change of variables for simplicity, l_ratio is the ration of the incident and outgoing photon wavelengths
         f = (
-            0.5 * r**2 * l_ratio**2 * 
+            0.5 * r**2 * l_ratio**2 *
             (l_ratio + 1 / l_ratio - (1 - x**2))
             )
-        return f 
-        
+        return f
+
     def get_max(): #finding the maximum value of the distribution for the envelope
         domain = [(i - 50) / 50 for i in range(101)]
         maximum = 0
@@ -102,7 +121,7 @@ def kn_rejacc(Ei): #rejection acceptance method since the pdf is univertible
             if diff_cross(i) > maximum:
                 maximum = diff_cross(i)
         return maximum
-    
+
     env = get_max() * 1.2 #envelope to bound distribution
     samplex = random() * 2 - 1
     sampley = random()
@@ -111,11 +130,10 @@ def kn_rejacc(Ei): #rejection acceptance method since the pdf is univertible
         sampley = random()
 #    if random() > 0.5:
 #        return -acos(samplex)
-#    else: 
+#    else:
 #        return -acos(samplex)
     return acos(samplex)
-    
-    
+
 
 class photon:
     def __init__(self, wlength, ipos, theta):
@@ -127,7 +145,14 @@ class photon:
         self.dir = rotate(vec(1,0,0), angle = self.theta, axis = vec(0,0,1))
         self.E_i = h * self.freq
         self.iM = h / self.wlength
-    
+        self.sum_K = 0
+        self.sum_eM = 0
+        self.sum_M_curr = 0
+        self.init_Ei_set = False
+        self.init_iM_set = False
+        self.init_Ei = 0
+        self.init_iM = 0
+
     def create_photon(self): #create photons based on initial angle and displacement
         self.curv = curve(color=vec(1-self.freq / 20,0,self.freq / 20))
         for i in range(400):
@@ -141,53 +166,51 @@ class photon:
         for i in range(self.curv.npoints):
             self.curv.color = vec(1-self.freq / 20,0,self.freq / 20)
             dx = (i - 200) * dl
-            t = self.localtime * c    
+            t = self.localtime * c
             y = dsine(dx, self.freq, t, 0)
             rotated = rotate(vec(dx + t, y, 0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1))
             self.curv.modify(i, pos = rotated + self.ipos)
         self.localtime += dt
         self.pos += rotate(vec(1,0,0), angle = diff_angle(self.dir,vec(1,0,0)), axis = vec(0,0,1)) * dt
-            
-    def collision(self):#calculating energy and updating frequency and electron velocity from conservation of energy and momentum
+
+    def collision(self): #calculating energy and updating frequency and electron velocity from conservation of energy and momentum
         self.E_f = self.E_i / (1 + (self.E_i / (m * c**2)) * (1-cos(self.theta)))
         self.K = self.E_i - self.E_f
         self.wlength += h /(m*c) * (1 - cos(self.theta))
         self.freq = c / self.wlength
         self.M = h / (self.wlength)
+        self.fM_x = self.M * cos(self.theta)
         self.dir = rotate(self.dir, angle = self.theta, axis = vec(0,0,1))
         self.localtime = 0
-        
-        
+
+
 class electron:
     def __init__(self, position):
         self.position = position
         self.p = vec(0,0,0) #initial momentum
-        
+
     def create_electron(self):
         self.sphere = sphere(pos=self.position, radius=1, color=color.cyan)
         return self.sphere
-        
+
     def electron_step(self):
         if mag(self.p) != 0:
             self.sphere.pos += mag(self.p) * c**2 / sqrt((mag(self.p) * c)**2 + (m*c**2)**2) * dt * norm(self.p)
-            
+
     def collision(self, in_dir, out_dir, E_i, E_f): #electron direction from conservation of momentum
         p_i = E_i / c * norm(in_dir)
         p_f = E_f / c * norm(out_dir)
         p_e = p_i - p_f
-        self.p += p_e 
-        
+        self.p += p_e
+
     def elec_collision(self, other):
-        global attached_vec
-        if attached_vec:
-            print(self.sphere.momentum)
         n = norm(self.sphere.pos - other.sphere.pos) #normal vector
         p_self_n = dot(self.p, n)
         p_other_n = dot(other.p, n)
         J = 0.5 * 2 * (m * p_self_n - m * p_other_n) / (m + m) * n #impulse along normal vector, divided by two since double counted (SHOULD FIX)
         self.p -= J
         other.p += J
- 
+
 #making electrons
 electrons = []
 
@@ -198,14 +221,13 @@ def click_electron(evt):
     e.create_electron()
     electrons.append(e)
 
-
 #making test photons
 photons = []
 for i in range(10):
     p = photon(c/freq, vec(-10, 2*(i - 5), 0), 0)
     p.create_photon()
     photons.append(p)
-        
+
 # widgets
 start = button(bind = run, text = 'run')
 reset = button(bind = reset, text = 'reset')
@@ -213,65 +235,53 @@ pause = button(bind = pauser, text = 'pause')
 frequency = slider(bind = change_freq, min = 1, max = 20, step = 1, value = freq)
 frequency_text = wtext(text = f'{freq}\n')
 click_text = wtext(text='Click on the canvas to place electrons, and then run')
+wl_label = wtext(text='')
+update_wl_label(c/freq)
 
 # checkbox, set mode
-    
+
 def change_freq(evt):
-    global freq 
+    global freq
     freq = evt.value
     frequency_text.text = f'{freq}\n'
-    
+    update_wl_label(c/freq)
+
 def reset():
-    global restart
-    global photons
-    global electrons
-    global sum_K
-    global sum_Ei_curr
-    global init_Ei 
+    global restart, photons, electrons, init_Ei, init_iM
     restart = True
     n = 0
     for phot in photons:
         phot.curv.clear()
         photons[n] = photon(c/freq, vec(-10, 2*(n - 5), 0), 0)
         photons[n].create_photon()
-        n+=1
+        n += 1
     for elect in electrons:
         elect.sphere.visible = False
         del elect
     electrons = []
-    
-    sum_K = 0
-    sum_Ei_curr = 0
+
     init_Ei = 0
-    
     init_iM = 0
-    sum_M_curr = 0
-    sum_M = 0
-    
+
     bar_Ei.data   = []
     bar_Ef.data   = []
     bar_Ke.data   = []
     bar_Etot.data = []
-    update_energy_graph(h * (c / (c/freq)), 0, 0)
-    
+    update_energy_graph(h * freq, 0, 0)
+
     bar_iM.data   = []
     bar_fM.data   = []
     bar_eM.data   = []
     bar_Mtot.data = []
-    update_momenta_graph(h / wlength, 0, 0) # fix this too
-        
+    update_momenta_graph(h * freq, 0, 0)
+    update_wl_label(c/freq)
+
 def pauser():
     global paused
     paused = True
 
 def run():
     global paused
-    global sum_K
-    global sum_Ei_curr
-    global init_Ei
-    global init_iM
-    global sum_M_curr
-    global sum_M
     paused = False
     while True:
         rate(1000)
@@ -280,34 +290,30 @@ def run():
             for elect in electrons:
                 distance_front = mag(phot.pos + phot.dir - elect.sphere.pos)  # detect a little in front of photon
                 if distance_front < elect.sphere.radius: #detect collision with electron
-                    
-                    #store incoming direction, change angle based on distribution, 
+
+                    #store incoming direction, change angle based on distribution,
                     #store outgoing direction and update energy and wavelength
                     in_dir = phot.dir
-                    
-                    if init_Ei == 0:    # keep track of initial photon Ei if its first
-                        init_Ei = phot.E_i
-                        
-                    if init_iM = 0:
-                        init_iM = phot.iM
-                    
+
+                    if not phot.init_Ei_set:    # keep track of initial photon Ei if its first
+                        phot.init_Ei = phot.E_i
+                        phot.init_iM = phot.iM
+                        phot.init_Ei_set = True
+                        phot.init_iM_set = True
+
                     phot.theta = kn_rejacc(phot.E_i)
                     phot.collision()
                     out_dir = phot.dir
-                    
-                    sum_K += phot.K
-                    sum_Ei_curr = phot.E_f # keep track of all affected electrons
-                    
-                    ### momenta update
-                    
-                    sum_M += phot.M
-                    sum_M_curr = phot.M
-                    
-                    
-                    update_energy_graph(init_Ei, sum_Ei_curr, sum_K)
-                    update_momenta_graph(init_iM, sum_M_curr, sum_M)
-                   
-                    #calculate electrons momentum from photon's momentum 
+
+                    phot.sum_K += phot.K
+                    phot.sum_eM += (phot.iM - phot.fM_x)
+                    phot.sum_M_curr = phot.fM_x # keep track of all affected electrons
+
+                    update_energy_graph(phot.init_Ei, phot.E_f, phot.sum_K)
+                    update_momenta_graph(phot.init_iM, phot.sum_M_curr, phot.sum_eM)
+                    update_wl_label(phot.wlength)
+
+                    #calculate electrons momentum from photon's momentum
                     #then update the photon's internal time and initial position
                     elect.collision(in_dir, out_dir, phot.E_i, phot.E_f)
                     phot.ipos = elect.sphere.pos + phot.dir # so we don't get repeated collisions we start it a little forward
@@ -318,7 +324,7 @@ def run():
                 if other is not self:
                     if mag(other.sphere.pos - self.sphere.pos) < 2 * self.sphere.radius:
                         self.elec_collision(other)
-            self.electron_step()  
+            self.electron_step()
         if restart == True:
             restart = False
             break
